7c2eb4f158807a07af335b428e6d5e7905fd7e2b
terminalforlife <terminalforlife@yahoo.com>
2022-03-14 02:25:51 +0000

    Partially but considerably optimize parsing
    This is incomplete because I don't have access to your 'TouchPad'
    device in order to see what I'm parsing, but this should still be a
    significant improvement.

--- scripts/inverse-scroll.sh
+++ scripts/inverse-scroll.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-# Get id of touchpad and the id of the field corresponding to
-# natural scrolling
-id=`xinput list | grep "TouchPad" | cut -d'=' -f2 | cut -d'[' -f1`
-natural_scrolling_id=`xinput list-props $id | \
-                      grep "Two-Finger Scrolling (" \
-                      | cut -d'(' -f2 | cut -d')' -f1`
-# echo $natural_scrolling_id
-# Set the property to true
-xinput --set-prop $id $natural_scrolling_id 1, 0
+while read -a Line; do
+	if [[ ${Line[*]} == *\ TouchPad\ * ]]; then
+		for Field in "${Line[@]}"; {
+			if [[ $Field == id=* ]]; then
+				natural_scrolling_id=`xinput list-props ${Field#*=} | \
+				                      grep "Two-Finger Scrolling (" \
+				                      | cut -d'(' -f2 | cut -d')' -f1`
+				# echo $natural_scrolling_id
+				# Set the property to true
+				xinput --set-prop $id $natural_scrolling_id 1, 0
+
+				break
+			fi
+		}
+
+		break
+	fi
+done <<< "$(xinput list)"
 
 #原
 #1, 0
